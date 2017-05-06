79553e2a3c51bec0f18b622138f82f567d5ab071
Oceanswave <sean.mclellan@sixconcepts.com>
2017-05-06 00:06:16 -0400

    Remove debug code

--- src/app/AppCtrl.js
+++ src/app/AppCtrl.js
@@ -3,7 +3,7 @@ class AppCtrl {
     this.$http = $http;
     if (this.$http === undefined)
       throw "mooseballs.";
-      
+
     this.url = 'https://github.com/beyond-sharepoint/sp-angular-webpack';
     this.workspaces = [
       {
@@ -16,16 +16,10 @@ class AppCtrl {
   }
 
   async doStuffAsync() {
-    console.log(this.$http);
-    console.log("fffoooooo");
-    var that = this;
-    if (this.$http === undefined)
-      throw "asdfasdfasfasdfasdfasdf";
-
     //Wrap call to $http in new promise, as $q != Promise
     //Don't return it either
     let response = await new Promise((resolve, reject) => {
-      that.$http({
+      this.$http({
         method: "GET",
         url: "http://www.example.com"
       }).then((r) => resolve(r), (err) => reject(err));
